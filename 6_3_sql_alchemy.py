94b889b6d1b9aa1464ea5ed9c5a2cd2ce0669a4b
Piotr Pięciak <piotr.pieciak@gmail.com>
2024-08-29 19:24:39 +0200

    Include select, update, delete, one additional function created

--- 6_3_sql_alchemy.py
+++ 6_3_sql_alchemy.py
@@ -1,6 +1,8 @@
 import csv
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, Float
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Float
+
+def advanced_data_display(show_it_to_me):
+    print(show_it_to_me)
 
 #____________PROGRAM GŁÓWNY__________________________
 if __name__ == "__main__":
@@ -43,7 +45,7 @@ if __name__ == "__main__":
             if line[0] == 'station':
                 continue
             conn.execute(ins, {'station' : line[0],'latitude' : line[1],'longitude' : line[2],'elevation' : line[3],'name' : line[4],'country' : line[5],'state' : line[6]} )
-
+    
 #kopiowanie danych z pliku clean_measure.csv do tabeli measures w stations.db
 #________________UWAGA w pliku jest ok 19500 wpisów - to może zająć kilka minut_____________
     ins = measures.insert()
@@ -56,7 +58,20 @@ if __name__ == "__main__":
             if line[0] == 'station':
                 continue
             conn.execute(ins, {'station' : line[0],'date' : line[1],'precip' : line[2],'tobs' : line[3]} )
+    
+#Zaznaczanie (select) wierszy z id < 4
+    sel = stations.select().where(stations.c.id < 4)
+    #print(conn.execute(sel).fetchall())
+    advanced_data_display(conn.execute(sel).fetchall())
+
+#Wykonanie update na tabeli stations, dla id == 4 zmieniamy name na Warszawa
+    update_st = stations.update().where(stations.c.id == 4).values(name="Warszawa")
+    conn.execute(update_st)
+
+#Wykonanie delete dla id = 2
+    delete_st = stations.delete().where(stations.c.id == 2)
+    conn.execute(delete_st)
 
 #Wydrukowanie końcowych komunikatów
-    print(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
-    print("stations.db was created and all data from files clean_measure.csv and clean_stations.csv were included to database. First five rows from table 'stations' were dislpayed")
+    advanced_data_display(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
+    advanced_data_display("stations.db was created and all data from files clean_measure.csv and clean_stations.csv were included to database. First five rows from table 'stations' were dislpayed. Functions like select, update and delete were included. ")
