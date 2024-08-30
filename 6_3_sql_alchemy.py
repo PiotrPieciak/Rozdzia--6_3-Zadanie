469c340ef846fac460f650a49ab441ec9494a89e
Piotr Pięciak <piotr.pieciak@gmail.com>
2024-08-30 16:54:42 +0200

    fix bug of table creation, additional functions

--- 6_3_sql_alchemy.py
+++ 6_3_sql_alchemy.py
@@ -1,8 +1,32 @@
 import csv
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Float
 
+#Funkcja do wyświetlania wyników 
 def advanced_data_display(show_it_to_me):
     print(show_it_to_me)
+
+#Funkcja wykonująca działanie na bazie danych
+def action_on_db(param):
+    conn.execute(param)
+
+#Funkcja kopiująca dane z plików csv do bazy danych
+def copy_csv_to_db(file_name):
+    if file_name == "clean_stations.csv":
+        ins = stations.insert()
+        ins.compile().params
+    else:
+        ins = measures.insert()
+        ins.compile().params
+
+    with open(file_name) as csv_file:
+        reader = csv.reader(csv_file)
+        for line in reader:
+            if line[0] == 'station':
+                continue
+            elif file_name == "clean_stations.csv":
+                conn.execute(ins, {'station' : line[0],'latitude' : line[1],'longitude' : line[2],'elevation' : line[3],'name' : line[4],'country' : line[5],'state' : line[6]} )
+            else:
+                conn.execute(ins, {'station' : line[0],'date' : line[1],'precip' : line[2],'tobs' : line[3]} )
 
 #____________PROGRAM GŁÓWNY__________________________
 if __name__ == "__main__":
@@ -11,7 +35,7 @@ if __name__ == "__main__":
     engine = create_engine('sqlite:///stations.db')
     meta = MetaData()
 
-#Utworzenie tabeli stations
+#definiujemy tabele stations
     stations = Table(
     'stations', meta,
     Column('id', Integer, primary_key=True),
@@ -24,7 +48,7 @@ if __name__ == "__main__":
     Column('state', String),
     )
 
-#Utworzenie tabeli measures
+#definiujemy tabele measures
     measures = Table(
     'measures', meta,
     Column('id', Integer, primary_key=True),
@@ -34,43 +58,30 @@ if __name__ == "__main__":
     Column('tobs', Integer),
     )
 
-#kopiowanie danych z pliku clean_stations.csv do tabeli stations w stations.db
-    ins = stations.insert()
-    ins.compile().params
+#Tworzymy tabele w bazie danych
+    meta.create_all(engine)
+
+#Pod zmienną conn podłączmy naszą bazę danych 
     conn = engine.connect()
 
-    with open("clean_stations.csv") as csv_file1:
-        reader = csv.reader(csv_file1)
-        for line in reader:
-            if line[0] == 'station':
-                continue
-            conn.execute(ins, {'station' : line[0],'latitude' : line[1],'longitude' : line[2],'elevation' : line[3],'name' : line[4],'country' : line[5],'state' : line[6]} )
-    
+#kopiowanie danych z pliku clean_stations.csv do tabeli stations w stations.db
+    copy_csv_to_db("clean_stations.csv")
+
 #kopiowanie danych z pliku clean_measure.csv do tabeli measures w stations.db
 #________________UWAGA w pliku jest ok 19500 wpisów - to może zająć kilka minut_____________
-    ins = measures.insert()
-    ins.compile().params
+    copy_csv_to_db("clean_measure.csv")
 
-    print("Please wait, it can takes few minutes...")
-    with open("clean_measure.csv") as csv_file2:
-        reader = csv.reader(csv_file2)
-        for line in reader:
-            if line[0] == 'station':
-                continue
-            conn.execute(ins, {'station' : line[0],'date' : line[1],'precip' : line[2],'tobs' : line[3]} )
-    
 #Zaznaczanie (select) wierszy z id < 4
     sel = stations.select().where(stations.c.id < 4)
-    #print(conn.execute(sel).fetchall())
     advanced_data_display(conn.execute(sel).fetchall())
 
 #Wykonanie update na tabeli stations, dla id == 4 zmieniamy name na Warszawa
     update_st = stations.update().where(stations.c.id == 4).values(name="Warszawa")
-    conn.execute(update_st)
-
+    action_on_db(update_st)
+    
 #Wykonanie delete dla id = 2
     delete_st = stations.delete().where(stations.c.id == 2)
-    conn.execute(delete_st)
+    action_on_db(delete_st)
 
 #Wydrukowanie końcowych komunikatów
     advanced_data_display(conn.execute("SELECT * FROM stations LIMIT 5").fetchall())
